de4b9119a952d43eb1fa61efd47a6f0486a461dc
hustle-dev <dlwoabsdk@gmail.com>
2022-10-10 02:10:04 +0900

    feat: pretendard 폰트 등록

--- src/screens/RootStack.tsx
+++ src/screens/RootStack.tsx
@@ -1,7 +1,7 @@
 // import {useNavigation} from '@react-navigation/native';
 import {NativeStackNavigationProp} from '@react-navigation/native-stack';
 import React from 'react';
-import {Text, View} from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
 
 type RootStackParamList = {
   MainTab: undefined;
@@ -15,11 +15,32 @@ export type RootStackNavigationProp =
 
 function RootStack() {
   //   const navigation = useNavigation<RootStackNavigationProp>();
+  //   console.log(styles.test);
+  //   console.log(styles.test2);
+
   return (
-    <View>
-      <Text>RootStack</Text>
+    <View style={styles.block}>
+      <Text style={styles.test}>안녕하세요 나는 줄리입니다.</Text>
+      <Text style={styles.test2}>안녕하세요 나는 줄리입니다.</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  block: {
+    marginTop: 50,
+  },
+  test: {
+    fontFamily: 'Pretendard',
+    fontSize: 20,
+    color: 'black',
+    textAlign: 'center',
+  },
+  test2: {
+    fontSize: 20,
+    color: 'black',
+    textAlign: 'center',
+  },
+});
 
 export default RootStack;
