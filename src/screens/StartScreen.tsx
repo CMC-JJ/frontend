c2f5d2dd891c94d8b4397c7ba746ca3769073123
moonchop <minssu913@naver.com>
2022-10-31 22:23:03 +0900

    :sparkles: start 페이지 구현 완료

--- src/screens/StartScreen.tsx
+++ src/screens/StartScreen.tsx
@@ -1,21 +1,51 @@
-import {StyleSheet, View} from 'react-native';
+import {Image, StyleSheet, View} from 'react-native';
 import React from 'react';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import {SignButton} from '../components';
 import FontText from '../components/FontText';
+import {RootStackNavigationProp} from './RootStack';
+import {useNavigation} from '@react-navigation/native';
 
 export default function StartScreen() {
+  const navigation = useNavigation<RootStackNavigationProp>();
   return (
     <SafeAreaView style={styles.container}>
-      <View style={{width: 100, height: 100, backgroundColor: 'yellow'}} />
+      <Image
+        style={{
+          marginTop: 80,
+          marginLeft: -9,
+        }}
+        source={require('../assets/images/logo.png')}
+      />
 
       <FontText style={styles.header}>
         {
           "'같이'의 가치를 실현하다\n한눈에 보고 간편하게 신청하는\n교통약자 서비스"
         }
       </FontText>
-      <SignButton style={styles.buttonGap} buttonText="로그인" isValid />
-      <SignButton buttonText="회원가입" isValid isbackgroundWhite />
+      <SignButton
+        style={styles.buttonGap}
+        buttonText="로그인"
+        isValid
+        onPress={() => navigation.navigate('SignIn')}
+      />
+      <SignButton
+        buttonText="회원가입"
+        isValid
+        isbackgroundWhite
+        onPress={() => navigation.navigate('SignUp')}
+      />
+      <View style={styles.footer}>
+        <FontText style={styles.text}>
+          {"'시작하기'를 누르는 것으로 "}
+          <FontText style={styles.textLine}>{'서비스 이용약관'}</FontText>
+          <FontText>{'과\n'}</FontText>
+          <FontText style={styles.textLine}>{'개인정보 취급 방침'}</FontText>
+          <FontText>
+            {'에 동의하고\n서비스를 이용하는 것으로 간주합니다.'}
+          </FontText>
+        </FontText>
+      </View>
     </SafeAreaView>
   );
 }
@@ -25,7 +55,7 @@ const styles = StyleSheet.create({
     flex: 1,
     paddingLeft: 25,
     paddingRight: 25,
-    marginTop: 80,
+    backgroundColor: 'white',
   },
   header: {
     fontWeight: '700',
@@ -35,6 +65,22 @@ const styles = StyleSheet.create({
     marginTop: 34,
   },
   buttonGap: {
+    marginTop: 105,
     marginBottom: 18,
+  },
+  text: {
+    textAlign: 'center',
+    color: '#878787',
+    lineHeight: 20,
+  },
+  textLine: {
+    color: 'black',
+    textDecorationLine: 'underline',
+  },
+  footer: {
+    position: 'absolute',
+    bottom: 30,
+    left: 0,
+    right: 0,
   },
 });
