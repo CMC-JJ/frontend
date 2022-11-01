b39cbb8b3f9ee9d098e2e7bf58f51e2031fc7f7b
moonchop <minssu913@naver.com>
2022-11-02 00:54:54 +0900

    :camera_flash: screen 폴더구조 변경전 스냅샷

--- src/screens/ServiceScreen.tsx
+++ src/screens/ServiceScreen.tsx
@@ -1,23 +1,11 @@
 import React from 'react';
 import {Text} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
-// import {useAuthStore} from '../store';
-// import {request} from '../utils';
 
 export function ServiceScreen() {
-  // const {auth} = useAuthStore();
-  // console.log(auth.jwtToken);
-  // const airpostList = async () =>
-  //   await request('web/airports', {Authorization: auth.jwtToken}, 'GET').then(
-  //     e => console.log(e),
-  //   );
-  // useEffect(() => {
-  //   airpostList();
-  // }, []);
   return (
     <SafeAreaView>
       <Text>서비스</Text>
-      {/* <Button title={'버튼'} onPress={airpostList}></Button> */}
     </SafeAreaView>
   );
 }
