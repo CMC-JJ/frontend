68ba47061ca498ad62b8fe2eed43bf4761efc197
hustle-dev <dlwoabsdk@gmail.com>
2022-11-10 20:16:14 +0900

    :lipstick: 아무 일정 없을 때 메인화면 구성

--- src/screens/BottomNavigation/Home/HomeScreen.tsx
+++ src/screens/BottomNavigation/Home/HomeScreen.tsx
@@ -1,12 +1,60 @@
 import {FontText} from '@/components';
 import {useAuthStore} from '@/store';
 import {request} from '@/utils';
+import {useNavigation} from '@react-navigation/native';
 import React, {useCallback, useEffect, useState} from 'react';
-import {Image, SafeAreaView, StatusBar, StyleSheet, View} from 'react-native';
+import {
+  Image,
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/Entypo';
+import type {MainTabNavigationProp} from '@/screens';
+
+type departureAirportService = {
+  airportServiceId: number;
+  name: string;
+  website: string;
+};
+type arrivalAirportService = {
+  airportServiceId: number;
+  name: string;
+  website: string;
+};
+type airlineService = {
+  airlineServiceId: number;
+  name: string;
+  website: string;
+};
+
+type Schedule = {
+  scheduleId: number;
+  airlineId: number;
+  airlineName: string;
+  arrivalAirportId: number;
+  arrivalAirportName: string;
+  departureAirportId: number;
+  departureAirportName: string;
+  leftDay: string;
+  scheduleName: string;
+  startAt: string;
+  departureAirportService: departureAirportService[];
+  arrivalAirportService: arrivalAirportService[];
+  airlineService: airlineService[];
+};
+
+type Data = {
+  schedule: Schedule;
+};
 
 export function HomeScreen() {
+  const navigation = useNavigation<MainTabNavigationProp>();
   const {auth} = useAuthStore();
-  const [data, setData] = useState();
+  const [data, setData] = useState<Data>();
 
   const fetchSchedule = useCallback(async () => {
     const result = await request(
@@ -37,29 +85,100 @@ export function HomeScreen() {
             style={styles.logoText}
           />
         </View>
-        <View style={styles.guideMessage}>
-          {data?.schedule ? (
-            <FontText style={styles.guideText}>
-              안녕하세요 {auth.nickName}! 오늘은 {data.schedule.scheduleName}{' '}
-              {data.schedule.leftDay}입니다.
-            </FontText>
-          ) : (
-            <FontText style={styles.guideText}>
-              안녕하세요 {auth.nickName}! 가치가자와 함께 떠나요!
-            </FontText>
-          )}
-        </View>
-        <View style={styles.serviceContainer}>
-          <View style={styles.serviceHeader}>
-            <Image
-              source={require('@/assets/images/calendar.png')}
-              style={styles.calendar}
-            />
-            <FontText style={styles.serviceHeaderText}>
-              제공받는 여행 서비스
-            </FontText>
+        <ScrollView>
+          <View style={styles.guideContainer}>
+            <View style={styles.guideMessage}>
+              {data?.schedule ? (
+                <View style={styles.guideTextContainer}>
+                  <FontText style={styles.greetMessage}>
+                    {'안녕하세요 '}
+                  </FontText>
+                  <FontText style={[styles.greetMessage, styles.nickName]}>
+                    {auth.nickName}!
+                  </FontText>
+                  {/* 오늘은 {data.schedule.scheduleName}{' '}
+                {data.schedule.leftDay}입니다. */}
+                </View>
+              ) : (
+                <View style={styles.guideTextContainer}>
+                  <FontText style={styles.greetMessage}>안녕하세요</FontText>
+                  <FontText style={[styles.greetMessage, styles.nickName]}>
+                    {auth.nickName}!
+                  </FontText>
+                  <View>
+                    <FontText style={styles.guideText}>
+                      가치가자와 함께
+                    </FontText>
+                    <FontText style={styles.guideText}>떠나요!</FontText>
+                  </View>
+                </View>
+              )}
+            </View>
+            <View style={styles.mainLogoContainer}>
+              <Image
+                source={require('@/assets/images/mainLogo.png')}
+                resizeMode="contain"
+                style={styles.mainLogo}
+              />
+            </View>
           </View>
-        </View>
+          <View style={styles.serviceContainer}>
+            <View style={styles.serviceHeader}>
+              <Image
+                source={require('@/assets/images/calendarShadow.png')}
+                style={styles.calendar}
+              />
+              <FontText style={styles.serviceHeaderText}>
+                제공받는 여행 서비스
+              </FontText>
+            </View>
+            {/* TODO: 데이터 있을때, 없을 때 구분! */}
+            <View style={styles.noScheduleContainer}>
+              <View style={styles.tempCircle} />
+              <View style={styles.noTextContainer}>
+                <FontText style={styles.noText}>현재 등록된 여행이</FontText>
+                <FontText style={styles.noText}>없습니다!</FontText>
+              </View>
+              <TouchableOpacity
+                style={styles.addSchedule}
+                onPress={() => {
+                  navigation.navigate('Schedule');
+                }}>
+                <FontText style={styles.addText}>여행 등록하기</FontText>
+                <Icon name="chevron-right" color="#0066FF" size={20} />
+              </TouchableOpacity>
+            </View>
+          </View>
+          <View style={[styles.serviceHeader, styles.serviceLocation]}>
+            <View style={styles.serviceTitle}>
+              <Image
+                source={require('@/assets/images/location.png')}
+                style={styles.calendar}
+              />
+              <FontText style={styles.serviceHeaderText}>
+                친구 위치보기
+              </FontText>
+            </View>
+            <View style={styles.selectOption}>
+              <FontText style={styles.selectText}>선택해주세요</FontText>
+              <Icon
+                style={styles.downIcon}
+                name="chevron-thin-down"
+                color="#0066FF"
+                size={15}
+              />
+            </View>
+          </View>
+          <View style={styles.mapContainer}>
+            <FontText style={styles.mapText}>내위치</FontText>
+            <TouchableOpacity style={styles.currentLocation}>
+              <Image
+                source={require('@/assets/images/currentLocation.png')}
+                style={styles.currentLocationIcon}
+              />
+            </TouchableOpacity>
+          </View>
+        </ScrollView>
       </View>
     </SafeAreaView>
   );
@@ -91,30 +210,165 @@ const styles = StyleSheet.create({
     height: 20,
     marginLeft: 7,
   },
-  guideMessage: {
-    marginTop: 40,
+  guideContainer: {
+    marginTop: 15,
+    flexDirection: 'row',
   },
-  guideText: {
-    width: 200,
-    fontWeight: '400',
-    fontSize: 26,
+  guideMessage: {
+    width: '65%',
+    marginTop: 13,
+  },
+  guideTextContainer: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+  },
+  greetMessage: {
+    fontWeight: '700',
+    fontSize: 24,
     lineHeight: 34,
   },
+  guideText: {
+    fontWeight: '400',
+    fontSize: 24,
+    lineHeight: 34,
+  },
+  nickName: {
+    color: '#0066FF',
+    marginLeft: 8,
+  },
+  mainLogoContainer: {
+    width: '30%',
+  },
+  mainLogo: {
+    width: 120,
+    height: 145,
+  },
   serviceContainer: {
-    marginTop: 40,
+    marginTop: 20,
   },
   serviceHeader: {
     flexDirection: 'row',
-    alignItems: 'center',
-  },
-  calendar: {
-    width: 30,
-    height: 30,
   },
   serviceHeaderText: {
     marginLeft: 2,
     fontWeight: '700',
     fontSize: 19,
     lineHeight: 28,
+  },
+  serviceTitle: {
+    flexDirection: 'row',
+  },
+  noScheduleContainer: {
+    marginTop: 45,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  tempCircle: {
+    width: 90,
+    height: 90,
+    borderRadius: 90,
+    backgroundColor: '#D9D9D9',
+  },
+  noTextContainer: {
+    marginTop: 18,
+    alignItems: 'center',
+  },
+  noText: {
+    fontWeight: '400',
+    fontSize: 15,
+    lineHeight: 23,
+  },
+  calendar: {
+    width: 30,
+    height: 30,
+  },
+  addSchedule: {
+    flexDirection: 'row',
+    marginTop: 20,
+
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  addText: {
+    marginRight: 2,
+    fontWeight: '700',
+    fontSize: 18,
+    lineHeight: 28,
+    color: '#0066FF',
+  },
+  serviceLocation: {
+    marginTop: 40,
+    justifyContent: 'space-between',
+  },
+  selectOption: {
+    width: 115,
+    height: 34,
+    marginTop: -5,
+    marginRight: 2,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+
+    borderRadius: 5,
+    backgroundColor: 'white',
+    shadowOffset: {width: 0, height: 2},
+    shadowColor: '#000000',
+    shadowOpacity: 0.15,
+    elevation: 2,
+  },
+  selectText: {
+    fontWeight: '700',
+    fontSize: 13,
+    lineHeight: 24,
+    color: '#0066FF',
+  },
+  downIcon: {
+    marginLeft: 4,
+  },
+  mapContainer: {
+    marginTop: 20,
+    marginHorizontal: 5,
+    marginBottom: 250,
+
+    height: 170,
+
+    borderRadius: 8,
+    backgroundColor: '#ffffff',
+    shadowOffset: {width: 0, height: 2},
+    shadowColor: '#000000',
+    shadowOpacity: 0.25,
+    elevation: 2,
+  },
+  mapText: {
+    marginTop: 12,
+    marginLeft: 18,
+
+    fontWeight: '700',
+    fontSize: 18,
+    lineHeight: 23,
+  },
+  currentLocation: {
+    position: 'absolute',
+    width: 38,
+    height: 38,
+
+    justifyContent: 'center',
+    alingItems: 'center',
+
+    right: 14,
+    bottom: 17,
+
+    borderRadius: 38,
+    backgroundColor: '#ffffff',
+    shadowOffset: {width: 0, height: 2},
+    shadowColor: '#000000',
+    shadowOpacity: 0.25,
+    elevation: 2,
+  },
+  currentLocationIcon: {
+    width: 20,
+    height: 20,
+
+    marginLeft: 9,
   },
 });
