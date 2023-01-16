7ef3662606bbfa5927fdfe288c298b6bc29e4ff0
hustle-dev <dlwoabsdk@gmail.com>
2023-01-16 21:59:51 +0900

    :recycle: airline API 모델 타입명 변경

--- src/models/airline.ts
+++ src/models/airline.ts
@@ -1,15 +1,15 @@
 import {BasicType, CommonReviewType} from './common';
 
-export type AirlineService = BasicType & {
+export type Airline = BasicType & {
   logoImageUrl: string;
   website: string;
 };
 
-export type AirlineListItem = Omit<AirlineService, 'website'>;
+export type AirlineListItem = Omit<Airline, 'website'>;
 
-export type AirlineServiceListItem = Pick<AirlineService, 'id' | 'name'>;
+export type AirlineServiceListItem = Pick<Airline, 'id' | 'name'>;
 
-export type AirlineDetailServiceItem = Omit<AirlineService, 'logoImageUrl'>;
+export type AirlineDetailServiceItem = Omit<Airline, 'logoImageUrl'>;
 
 export type AirlineDetail = {
   airlineId: number;
