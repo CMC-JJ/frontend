64d3b2638b8de1e1caf2a7033e967a176ca259f1
hustle-dev <dlwoabsdk@gmail.com>
2023-01-16 22:06:58 +0900

    :sparkles: airport API 모델 타입 작성

---
+++ src/models/airport.ts
@@ -0,0 +1,28 @@
+import {BasicType, CommonReviewType} from './common';
+
+export type Airport = BasicType & {
+  region: string;
+  website: string;
+};
+
+export type AirportListItem = Omit<Airport, 'website'>;
+
+export type AirportServiceListItem = Pick<Airport, 'id' | 'name'>;
+
+export type AirportDetailServiceItem = Omit<Airport, 'region'>;
+
+export type AirportDetail = {
+  airportId: number;
+  airportName: string;
+  customerServiceNumber: string;
+  website: string;
+  avgReview: string;
+  availableAt: string;
+  airportServices: AirportDetailServiceItem[];
+};
+
+export type AirportReviewListItem = CommonReviewType & {
+  airportReviewId: number;
+  nickName: string;
+  reviewedAirportServices: string[];
+};
--- src/models/index.ts
+++ src/models/index.ts
@@ -1,2 +1,3 @@
 export * from './user';
 export * from './airline';
+export * from './airport';
